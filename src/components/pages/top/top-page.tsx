de60ac7cce625e486ea2422d7b194aede0dcc35d
kobaru <ta.a.1445.1@gmail.com>
2022-07-15 02:10:43 +0900

    [WIP] 仮表示

--- src/components/pages/top/top-page.tsx
+++ src/components/pages/top/top-page.tsx
@@ -1,23 +1,45 @@
-import React, { useState, useCallback, useEffect } from "react"
+import React, { useState, useCallback, useEffect } from "react";
+import Chart from 'chart.js/auto';
+
+const drawChart = (json: any) => {
+  const mydata = {
+    labels: json.daily.time,
+    datasets: [
+      {
+        label: "最高気温",
+        data: json.daily.temperature_2m_max,
+        borderColor: "rgb(192, 75, 75)",
+      },
+      {
+        label: "最低気温",
+        data: json.daily.temperature_2m_min,
+        borderColor: "rgb(75, 75, 192)",
+      },
+    ],
+  };
+
+  const doc = document.getElementById('canvas') as HTMLCanvasElement;
+
+
+  new Chart(doc, {
+    type: "line",
+    data: mydata,
+  });
+};
+
 
 export const TopPage = () => {
-  const [text , setText] = useState("");
-  const handleInputChange = useCallback(
-    (e: any) => {
-      setText(e.target.value);
-    },
-    [text]
-  );
+  const url =
+    "https://api.open-meteo.com/v1/forecast?latitude=35.6785&longitude=139.6823&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min&timezone=Asia%2FTokyo";
 
-  useEffect(() => {
-    console.log("text");
-  }, [text]);
-
+  fetch(url)
+    .then((data) => data.json())
+    .then((json) => drawChart(json));
 
   return (
-    <>
-      <input type="text" value={text} onChange={handleInputChange} />
-      <h1>入力表示：{text}</h1>
-    </>
-  )
-}
+    <div>
+      <div id="App">天気</div>
+      <canvas width="1280" height="720" id="canvas"></canvas>
+    </div>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import createRoot from 'react-dom';
 import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 
-ReactDOM.render(
+createRoot.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
