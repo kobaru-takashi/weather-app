72b2525b2ab30143a0344e3d4a8ae163dadcec8d
kobaru <ta.a.1445.1@gmail.com>
2022-07-26 04:45:28 +0900

    [Add] input　追加

--- src/components/pages/data/area-url.ts
+++ src/components/pages/data/area-url.ts
@@ -20,3 +20,7 @@ export const TokyoP =
 "https://api.open-meteo.com/v1/forecast?latitude=35.6785&longitude=139.6823&daily=precipitation_sum&timezone=Asia%2FTokyo";
 export const New_YorkP =
 "https://api.open-meteo.com/v1/forecast?latitude=40.71&longitude=-74.01&daily=precipitation_sum&timezone=America%2FNew_York";
+export const TokyoHST =
+"https://api.open-meteo.com/v1/forecast?latitude=35.6785&longitude=139.6823&daily=temperature_2m_max,apparent_temperature_max&current_weather=true&timezone=Asia%2FTokyo&start_date=2022-07-01&end_date=2022-07-31";
+export const TokyoLST =
+"https://api.open-meteo.com/v1/forecast?latitude=35.6785&longitude=139.6823&daily=temperature_2m_min,apparent_temperature_min&current_weather=true&timezone=Asia%2FTokyo&start_date=2022-07-01&end_date=2022-07-31";
--- src/components/pages/top/top-page.tsx
+++ src/components/pages/top/top-page.tsx
@@ -7,18 +7,70 @@ import {
   New_YorkLT,
   New_YorkP,
   TokyoHG,
+  TokyoHST,
   TokyoHT,
   TokyoHTAndLT,
   TokyoHWV,
   TokyoHWVAndHG,
+  TokyoLST,
   TokyoLT,
   TokyoP,
 } from "../data/area-url";
+import { Input } from "../../ui/input";
 
 export const TopPage = () => {
   const [temperatureChart, setTemperatureChart] = useState<Chart | null>(null);
   const [windVelocityChart, setWindVelocityChart] = useState<Chart | null>(null);
   const [precipitationChart, setPrecipitationChart] = useState<Chart | null>(null);
+  const [sensoryTemperatureChart, setSensoryTemperatureChart] = useState<Chart | null>(null);
+  const [temperatureInputChart, setTemperatureInputChart] = useState<Chart | null>(null);
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+
+  const changeStartDate = useCallback((v: string) => {
+    setStartDate(v);
+  }, [startDate]);
+  const changeEndDate = useCallback((v: string) => {
+    setEndDate(v);
+  }, [endDate]);
+
+  // const drawChart = (
+  //   labels: any,
+  //   label1: string,
+  //   data1: any,
+  //   label2: string,
+  //   data2: any,
+  //   table: string,
+  //   chart: Chart | null,
+  //   chartCallback: React.Dispatch<React.SetStateAction<Chart | null>>
+  // ) => {
+  //   const myData = {
+  //     labels: labels,
+  //     datasets: [
+  //       {
+  //         label: label1,
+  //         data: data1,
+  //         borderColor: "rgb(192, 75, 75)",
+  //       },
+  //       {
+  //         label: label2,
+  //         data: data2,
+  //         borderColor: "rgb(75, 75, 192)",
+  //       },
+  //     ],
+  //   };
+
+  //   if (chart) {
+  //     chart.destroy();
+  //   }
+
+  //   chartCallback(
+  //     new Chart(table, {
+  //       type: "line",
+  //       data: myData,
+  //     })
+  //   );
+  // };
 
   const drawChartTemperature = (json: any) => {
     const myData = {
@@ -78,7 +130,6 @@ export const TopPage = () => {
     );
   };
 
-
   const drawChartPrecipitation = (json: any) => {
     const myData = {
       labels: json.daily.time,
@@ -103,26 +154,105 @@ export const TopPage = () => {
     );
   };
 
-  const onChangeTemperatureArea = (url: string) => {
-    fetch(url)
-      .then((data) => data.json())
-      .then((json) => drawChartTemperature(json));
+  const drawChartSensoryTemperatureH = (json: any) => {
+    const myData = {
+      labels: json.daily.time,
+      datasets: [
+        {
+          label: "最高気温",
+          data: json.daily.temperature_2m_max,
+          borderColor: "rgb(192, 75, 75)",
+        },
+        {
+          label: "最高体感気温",
+          data: json.daily.apparent_temperature_max,
+          borderColor: "rgb(75, 75, 192)",
+        },
+      ],
+    };
+    const table = "sensoryTemperature";
+
+    if (sensoryTemperatureChart) {
+      sensoryTemperatureChart.destroy();
+    }
+    setSensoryTemperatureChart(
+      new Chart(table, {
+        type: "line",
+        data: myData,
+      })
+    );
   };
-  const onChangeWindVelocityArea = (url: string) => {
-    fetch(url)
-      .then((data) => data.json())
-      .then((json) => drawChartWindVelocity(json));
+
+  const drawChartSensoryTemperatureL = (json: any) => {
+    const myData = {
+      labels: json.daily.time,
+      datasets: [
+        {
+          label: "最低気温",
+          data: json.daily.temperature_2m_min,
+          borderColor: "rgb(192, 75, 75)",
+        },
+        {
+          label: "最低体感気温",
+          data: json.daily.apparent_temperature_min,
+          borderColor: "rgb(75, 75, 192)",
+        },
+      ],
+    };
+    const table = "sensoryTemperature";
+
+    if (sensoryTemperatureChart) {
+      sensoryTemperatureChart.destroy();
+    }
+    setSensoryTemperatureChart(
+      new Chart(table, {
+        type: "line",
+        data: myData,
+      })
+    );
   };
-  const onChangePrecipitationArea = (url: string) => {
+
+  const drawChartTemperatureInput = (json: any) => {
+    const myData = {
+      labels: json.daily.time,
+      datasets: [
+        {
+          label: "最高気温",
+          data: json.daily.temperature_2m_max,
+          borderColor: "rgb(192, 75, 75)",
+        },
+      ],
+    };
+    const table = "temperatureInput";
+
+    if (temperatureInputChart) {
+      temperatureInputChart.destroy();
+    }
+    setTemperatureInputChart(
+      new Chart(table, {
+        type: "line",
+        data: myData,
+      })
+    );
+  };
+
+  const onChangeJsonData = (url: string, callback: (jsonData: string) => void) => {
     fetch(url)
       .then((data) => data.json())
-      .then((json) => drawChartPrecipitation(json));
+      .then((json) => callback(json));
+  };
+  const onChangeTemperatureInput = (start: string, end: string) => {
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=35.6785&longitude=139.6823&daily=temperature_2m_max&current_weather=true&timezone=Asia%2FTokyo&start_date=2022-07-${start}&end_date=2022-07-${end}`;
+    fetch(url)
+      .then((data) => data.json())
+      .then((json) => drawChartTemperatureInput(json));
   };
 
   useEffect(() => {
-    onChangeTemperatureArea(New_YorkHTAndLT);
-    onChangeWindVelocityArea(TokyoHWVAndHG);
-    onChangePrecipitationArea(New_YorkP);
+    onChangeJsonData(New_YorkHTAndLT, drawChartTemperature);
+    onChangeJsonData(TokyoHWVAndHG, drawChartWindVelocity);
+    onChangeJsonData(New_YorkP, drawChartPrecipitation);
+    onChangeJsonData(TokyoHST, drawChartSensoryTemperatureH);
   }, []);
 
   return (
@@ -133,22 +263,28 @@ export const TopPage = () => {
       </div>
       <Button
         label="ニューヨーク最高気温＆最低気温"
-        onClick={() => onChangeTemperatureArea(New_YorkHTAndLT)}
+        onClick={() => onChangeJsonData(New_YorkHTAndLT, drawChartTemperature)}
       />
       <Button
         label="ニューヨーク最高気温"
-        onClick={() => onChangeTemperatureArea(New_YorkHT)}
+        onClick={() => onChangeJsonData(New_YorkHT, drawChartTemperature)}
       />
       <Button
         label="ニューヨーク最低気温"
-        onClick={() => onChangeTemperatureArea(New_YorkLT)}
+        onClick={() => onChangeJsonData(New_YorkLT, drawChartTemperature)}
       />
       <Button
         label="東京最高気温＆最低気温"
-        onClick={() => onChangeTemperatureArea(TokyoHTAndLT)}
+        onClick={() => onChangeJsonData(TokyoHTAndLT, drawChartTemperature)}
       />
-      <Button label="東京最高気温" onClick={() => onChangeTemperatureArea(TokyoHT)} />
-      <Button label="東京最低気温" onClick={() => onChangeTemperatureArea(TokyoLT)} />
+      <Button
+        label="東京最高気温"
+        onClick={() => onChangeJsonData(TokyoHT, drawChartTemperature)}
+      />
+      <Button
+        label="東京最低気温"
+        onClick={() => onChangeJsonData(TokyoLT, drawChartTemperature)}
+      />
 
       <div>風速</div>
       <div id="chartReport">
@@ -156,17 +292,44 @@ export const TopPage = () => {
       </div>
       <Button
         label="東京最高風速＆突風"
-        onClick={() => onChangeWindVelocityArea(TokyoHWVAndHG)}
+        onClick={() => onChangeJsonData(TokyoHWVAndHG, drawChartWindVelocity)}
       />
-      <Button label="東京最高風速" onClick={() => onChangeWindVelocityArea(TokyoHWV)} />
-      <Button label="東京最低突風" onClick={() => onChangeWindVelocityArea(TokyoHG)} />
+      <Button
+        label="東京最高風速"
+        onClick={() => onChangeJsonData(TokyoHWV, drawChartWindVelocity)}
+      />
+      <Button
+        label="東京最低突風"
+        onClick={() => onChangeJsonData(TokyoHG, drawChartWindVelocity)}
+      />
 
       <div>降水量の合計</div>
       <div id="chartReport">
         <canvas id="precipitation"></canvas>
       </div>
-      <Button label="ニューヨーク降水量" onClick={() => onChangePrecipitationArea(New_YorkP)} />
-      <Button label="東京降水量" onClick={() => onChangePrecipitationArea(TokyoP)} />
+      <Button
+        label="ニューヨーク降水量"
+        onClick={() => onChangeJsonData(New_YorkP, drawChartPrecipitation)}
+      />
+      <Button
+        label="東京降水量"
+        onClick={() => onChangeJsonData(TokyoP, drawChartPrecipitation)}
+      />
+
+      <div>気温と体感気温比較</div>
+      <div id="chartReport">
+        <canvas id="sensoryTemperature"></canvas>
+      </div>
+      <Button label="最高体感気温" onClick={() => onChangeJsonData(TokyoHST, drawChartSensoryTemperatureH)} />
+      <Button label="最低体感気温" onClick={() => onChangeJsonData(TokyoLST, drawChartSensoryTemperatureL)} />
+
+      <div>最高気温</div>
+      <div id="chartReport">
+        <canvas id="temperatureInput"></canvas>
+      </div>
+      <Button label="最高体感気温" onClick={() => onChangeTemperatureInput(startDate, endDate)} />
+      <Input placeholder="開始日" maxLength={2} onChange={(e) => changeStartDate(e.target.value)}/>
+      <Input placeholder="終了日" maxLength={2} onChange={(e) => changeEndDate(e.target.value)}/>
     </div>
   );
 };
